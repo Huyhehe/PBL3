74381e85e239cd3126695aa86bd01ef58ccfa02e
Huyhehe <thhyy2002@gmail.com>
2022-05-02 21:28:10 +0700

    Babel config change

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "PBL3/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/PBL3/" : "/",
   presets: ["@vue/cli-plugin-babel/preset"],
 };
