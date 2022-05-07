166c18dc853e5d7b5242a1c5b9aa14f52d42b49f
Huyhehe <thhyy2002@gmail.com>
2022-05-07 09:15:09 +0700

    restore

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/PBL3/" : "/",
+  // mode: "production",
+  // publicPath: process.env.NODE_ENV === "production" ? "/PBL3/" : "/",
   presets: ["@vue/cli-plugin-babel/preset"],
 };
